f24a48cbb3923e0cf7cdf4cb2ad302b0e2871d01
bozitoma <64360278+bozitoma@users.noreply.github.com>
2023-11-03 12:13:47 +0900

    Add files via upload

---
+++ src/types/replicant.ts
@@ -0,0 +1,13 @@
+// Replicantsの型を定義
+export interface ReplicantMap {
+  // Sample
+  // playerName1p: string;
+  // playerName2p: string;
+}
+
+// Replicantsの初期値を定義
+export const replicantDefaultValues: ReplicantMap = {
+  // Sample
+  // playerName1p: '',
+  // playerName2p: '',
+};
