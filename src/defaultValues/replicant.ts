d89676b238fb09f2c7c8d11b2110bd7196d93c1c
bozitoma <bozitoma@gmail.com>
2024-10-06 16:52:11 +0900

    デフォルトバリューのディレクトリを変更

---
+++ src/defaultValues/replicant.ts
@@ -0,0 +1,8 @@
+import { ReplicantMap } from "../types/replicant";
+
+// Replicantsの初期値を定義
+export const replicantDefaultValues: ReplicantMap = {
+  // Sample
+  yugioh: []
+  // playerName2p: '',
+};
--- src/types/replicant.d.ts
+++ src/types/replicant.d.ts
@@ -6,10 +6,3 @@ export interface ReplicantMap {
   }[];
   // playerName2p: string;
 }
-
-// Replicantsの初期値を定義
-export const replicantDefaultValues: ReplicantMap = {
-  // Sample
-  yugioh: [],
-  // playerName2p: '',
-};
